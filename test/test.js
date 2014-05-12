6d0027c3e993928c4bb43447b09bef1682b3f70f
Dan Smith <dotherightthing@users.noreply.github.com>
2014-05-12 14:02:07 +1200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -54,13 +54,14 @@ module("Default state");
 
 	});
 
-	test("After run, items are height matched", function() {
+	test("After run, items are height matched, by reading height and setting height", function() {
 
 		// Setup
 		var $match = $('.short').add( $('.medium') ).add( $('.tall') );
 		$match.matchHeights({
 			read: 'height',
-			write: 'height'
+			write: 'height',
+			animation_duration_ms: 0
 		});
 
 		var short_style = $('.short').attr('style');
@@ -97,13 +98,102 @@ module("Default state");
 
 	});
 
-	test("After run, items are outerHeight matched", function() {
+	test("After run, items are height matched, by reading outerHeight and setting height", function() {
 
 		// Setup
 		var $match = $('.short').add( $('.medium') ).add( $('.tall') );
 		$match.matchHeights({
 			read: 'outerHeight',
-			write: 'min-height'
+			write: 'height',
+			animation_duration_ms: 0
+		});
+
+		var short_style = $('.short').attr('style');
+		var medium_style = $('.medium').attr('style');
+		var tall_style = $('.tall').attr('style');
+
+		var short_height = $('.short').outerHeight();
+		var medium_height = $('.medium').outerHeight();
+		var tall_height = $('.tall').outerHeight();
+
+		equal(
+			( short_style !== undefined && medium_style !== undefined && tall_style !== undefined ),
+			true,
+			'Style attributes added'
+		);
+
+		equal(
+			( short_height === medium_height ),
+			true,
+			'.short height (' + short_height + 'px) matches .medium height (' + medium_height + 'px) ... (' + short_style + ' vs ' + medium_style + ')'
+		);
+
+		equal(
+			( medium_height === tall_height ),
+			true,
+			'.medium height (' + medium_height + 'px) matches .tall height (' + tall_height + 'px) ... (' + medium_style + ' vs ' + tall_style + ')'
+		);
+
+		equal(
+			( short_height === tall_height ),
+			true,
+			'.short height (' + short_height + 'px) matches .tall height (' + tall_height + 'px) ... (' + short_style + ' vs ' + tall_style + ')'
+		);
+
+	});
+
+	test("After run, items are height matched, by reading height and setting min-height", function() {
+
+		// Setup
+		var $match = $('.short').add( $('.medium') ).add( $('.tall') );
+		$match.matchHeights({
+			read: 'height',
+			write: 'min-height',
+			animation_duration_ms: 0 // else test fails as height is read before the new height as been transitioned to
+		});
+
+		var short_style = $('.short').attr('style');
+		var medium_style = $('.medium').attr('style');
+		var tall_style = $('.tall').attr('style');
+
+		var short_height = $('.short').height();
+		var medium_height = $('.medium').height();
+		var tall_height = $('.tall').height();
+
+		equal(
+			( short_style !== undefined && medium_style !== undefined && tall_style !== undefined ),
+			true,
+			'Style attributes added'
+		);
+
+		equal(
+			( short_height === medium_height ),
+			true,
+			'.short height (' + short_height + 'px) matches .medium height (' + medium_height + 'px) ... (' + short_style + ' vs ' + medium_style + ')'
+		);
+
+		equal(
+			( medium_height === tall_height ),
+			true,
+			'.medium height (' + medium_height + 'px) matches .tall height (' + tall_height + 'px) ... (' + medium_style + ' vs ' + tall_style + ')'
+		);
+
+		equal(
+			( short_height === tall_height ),
+			true,
+			'.short height (' + short_height + 'px) matches .tall height (' + tall_height + 'px) ... (' + short_style + ' vs ' + tall_style + ')'
+		);
+
+	});
+
+	test("After run, items are height matched, by reading outerHeight and setting min-height", function() {
+
+		// Setup
+		var $match = $('.short').add( $('.medium') ).add( $('.tall') );
+		$match.matchHeights({
+			read: 'outerHeight',
+			write: 'min-height',
+			animation_duration_ms: 0 // else test fails as height is read before the new height as been transitioned to
 		});
 
 		var short_style = $('.short').attr('style');
@@ -143,7 +233,7 @@ module("Default state");
 module("Destroy");
 
 	//test("$('" + testvars.treeitem + "').tree()", function() {
-	test("Markup reverts to default state (TODO: add destroy method)", function() {
+	test("Markup reverts to default state", function() {
 
 		// Setup
 		var $match = $('.short').add( $('.medium') ).add( $('.tall') );
